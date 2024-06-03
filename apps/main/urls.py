237a91a86e09be937634b067cac71a415dc249e2
Mozdalif sikder <mozsocia@gmail.com>
2024-06-03 18:10:43 +0600

    tuto jobs list, details

--- apps/main/urls.py
+++ apps/main/urls.py
@@ -11,4 +11,8 @@ urlpatterns = [
 
     path('all-tutors/', teacher_list, name='all_tutors'),
     path('tutors/<int:pk>/', teacher_detail, name='tutor_detail'),
+
+    path('job-requirements/', job_requirement_list, name='job_requirement_list'),
+    path('job-requirements/<int:pk>/', job_requirement_detail, name='job_requirement_detail'),
 ]
+
--- apps/main/views.py
+++ apps/main/views.py
@@ -43,3 +43,29 @@ def teacher_list(request):
 def teacher_detail(request, pk):
     teacher = get_object_or_404(Teacher, pk=pk)
     return render(request, 'site/page/tutor_detail.html', {'teacher': teacher})
+
+
+
+
+
+from django.shortcuts import render, get_object_or_404
+from apps.student.models import *
+
+
+def job_requirement_list(request):
+    job_requirements = JobRequirement.objects.all()
+    page = request.GET.get('page', 1)
+    paginator = Paginator(job_requirements, 10)  # Show 10 job requirements per page
+
+    try:
+        job_requirements_paginated = paginator.page(page)
+    except PageNotAnInteger:
+        job_requirements_paginated = paginator.page(1)
+    except EmptyPage:
+        job_requirements_paginated = paginator.page(paginator.num_pages)
+
+    return render(request, 'site/page/job_requirements_list.html', {'job_requirements': job_requirements_paginated})
+
+def job_requirement_detail(request, pk):
+    job_requirement = get_object_or_404(JobRequirement, pk=pk)
+    return render(request, 'site/page/job_requirements_detail.html', { 'job_requirement': job_requirement })
